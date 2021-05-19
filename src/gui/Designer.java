d7643d6aae9fc46afd7aa9e9c68a6d0203586e2e
Markus Telser <markus.telser99@gmail.com>
2021-05-19 09:05:16 +0200

    fixed toolbar selection blue line bug

--- src/gui/Designer.java
+++ src/gui/Designer.java
@@ -8,29 +8,31 @@ import javax.swing.border.LineBorder;
 public class Designer {
 	private static Color foregroundColor = Color.BLACK;
 	private static Color backgroundColor = Color.WHITE;
-	private static Color selectedForegroundColor = Color.BLACK;
-	private static Color selectedBackgroundColor = new Color(0, 255, 0);
-	
+	private static Color selectedForegroundColor = Color.WHITE;
+	private static Color selectedBackgroundColor = new Color(50, 50, 50);
+
 	public static void initialize() {
 		UIManager.put("MenuBar.foreground", foregroundColor);
 		UIManager.put("MenuBar.background", backgroundColor);
-		
-	    //menu settings
-	    UIManager.put("Menu.foreground", foregroundColor);
-	    UIManager.put("Menu.background", backgroundColor);
-	    UIManager.put("Menu.selectionForeground",selectedForegroundColor);
-	    UIManager.put("Menu.selectionBackground", selectedBackgroundColor);
-	    UIManager.put("Menu.opaque", true);
-	    
-	    //menu item settings
-	    UIManager.put("MenuItem.background", backgroundColor);
-	    UIManager.put("MenuItem.foreground", foregroundColor);
-	    UIManager.put("MenuItem.selectionForeground", selectedForegroundColor);
-	    UIManager.put("MenuItem.selectionBackground", selectedBackgroundColor);
-	    UIManager.put("MenuItem.acceleratorForeground", selectedForegroundColor);
-	    UIManager.put("MenuItem.acceleratorSelectionForeground", foregroundColor);
-	    
-	    //pop up menu settings
-	    UIManager.put("PopupMenu.border", new LineBorder(Color.BLACK));
+
+		// menu settings
+		UIManager.put("Menu.foreground", foregroundColor);
+		UIManager.put("Menu.background", backgroundColor);
+		UIManager.put("Menu.selectionForeground", selectedForegroundColor);
+		UIManager.put("Menu.selectionBackground", selectedBackgroundColor);
+		UIManager.put("Menu.opaque", true);
+
+		// menu item settings
+		UIManager.put("MenuItem.background", backgroundColor);
+		UIManager.put("MenuItem.foreground", foregroundColor);
+		UIManager.put("MenuItem.selectionForeground", selectedForegroundColor);
+		UIManager.put("MenuItem.selectionBackground", selectedBackgroundColor);
+		UIManager.put("MenuItem.acceleratorForeground", selectedForegroundColor);
+		UIManager.put("MenuItem.acceleratorSelectionForeground", foregroundColor);
+		UIManager.put("MenuItem.borderPainted", false);
+
+		// pop up menu settings
+		UIManager.put("PopupMenu.border", new LineBorder(Color.BLACK));
+		UIManager.put("Separator.foreground", foregroundColor);
 	}
 }
